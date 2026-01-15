e36f8f4348fc3414a0049d6e1029c292b5dee16b
themonicah <themonicah@gmail.com>
2026-01-15 19:35:44 +0100

    Fix router basename for /dutch900 path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/dutch900">
       <App />
     </BrowserRouter>
   </StrictMode>,
